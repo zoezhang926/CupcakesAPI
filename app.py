1de0ea2d38490e6427353f6afa80dc138a3e30f8
zoezhang926 <zoezhang926@gmail.com>
2020-08-26 22:28:41 -0400

    add model

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db,connect_db,Cupcake,Default_Image
+# from form import AddPetForm,EditPetForm
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "dogsarethebest1120"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,22 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+Default_Image="https://tinyurl.com/demo-cupcake"
+
+db = SQLAlchemy()
+
+### models go below !
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    "CupcateModel"
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text,nullable=False)
+    size = db.Column(db.Text,nullable=False)
+    rating = db.Column(db.Text,nullable=False)
+    flavor = db.Column(db.Text,nullable=False)
